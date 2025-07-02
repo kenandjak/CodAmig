135e67e87f07263cdcfc0b9c5453184d825afb0e
kenandjak <100363820+kenandjak@users.noreply.github.com>
2025-07-01 21:43:05 -0300

    refactor: improve `Home` page

--- codamig/src/view/pages/Home.tsx
+++ codamig/src/view/pages/Home.tsx
@@ -2,47 +2,37 @@
 import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import SubjectButton from "../components/SubjectButton";
+
+type Subject = {
+  id: number;
+  subject: string;
+  level: string;
+  className?: string;
+};
+const subjectsData: Subject[] = [
+  { id: 1, subject: "C++", level: "Opcional" },
+  { id: 2, subject: "Introdução", level: "Básico" },
+  { id: 3, subject: "Strings", level: "Básico" },
+  { id: 4, subject: "Busca e Ordenação", level: "Regular" },
+  { id: 5, subject: "Grafos", level: "Intermediário" },
+  { id: 6, subject: "Dynamic Program", level: "Intermediário" },
+];
 function Home() {
   const navigate = useNavigate();
   return (
     <div className="min-h-screen bg-gradient-purple-dark flex flex-col items-center">
       <Header />
-      <SubjectButton
-        className="mt-20"
-        onClick={() => navigate("/")}
-        subject="C++"
-        level="Opcional"
-      />
-      <SubjectButton
-        className="mt-20 text-xl"
-        onClick={() => navigate("/")}
-        subject="Introdução"
-        level="Básico"
-      />
-      <SubjectButton
-        className="mt-20"
-        onClick={() => navigate("/")}
-        subject="Strings"
-        level="Básico"
-      />
-      <SubjectButton
-        className="mt-20"
-        onClick={() => navigate("/")}
-        subject="Busca e Ordenação"
-        level="Regular"
-      />
-      <SubjectButton
-        className="mt-20"
-        onClick={() => navigate("/")}
-        subject="Grafos"
-        level="Intermediário"
-      />
-      <SubjectButton
-        className="mt-20"
-        onClick={() => navigate("/")}
-        subject="Dynamic Program"
-        level="Intermediário"
-      />
+      <div className="mt-20 flex flex-col items-center gap-20">
+        {subjectsData.map((subjectItem) => (
+          <SubjectButton
+            key={subjectItem.id}
+            onClick={() => navigate("/")}
+            subject={subjectItem.subject}
+            level={subjectItem.level}
+            className={subjectItem.className}
+          />
+        ))}
+      </div>
     </div>
   );
 }
